edf2a06ed46db5492f36d58f17b5d3fe64611d09
Sofiya Semenova <novasofiya@gmail.com>
2016-10-15 16:39:47 -0400

    Canvas

--- assets/js.js
+++ assets/js.js
@@ -1,17 +1,93 @@
-new Vue({
+var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!',
         curves: ["bla!", "bla2", "bla3"],
-        attributes: ["x-shift", "y-shift", "rotation", "x-stretch", "y-stretch"]
+        attributes: [{
+                name:"x-shift",
+                value: 50
+            },
+            {
+                name: "y-shift",
+                value: 50
+            },
+            {
+                name: "rotation",
+                value: 50
+            },
+            {
+                name: "x-stretch",
+                value: 50
+            },
+            {
+                name: "y-stretch",
+                value: 50
+            }],
+        binArray: []
     },
     methods: {
-        readFile: function() {
-            var reader = require("fs");
-            reader.readFile("../renderer/data", function(text){
-                var textByLine = text.split(",")
-            });
-            console.log(textByLine);
+        drawCanvas: function() {
+            var rows = 520, //height
+                cols = 504, //width
+                canvas = document.getElementById("myCanvas"),
+                dataSplit = reshape(this.binArray, rows, cols),
+                ctx = canvas.getContext("2d");
+
+            var imgData = ctx.createImageData(cols, rows);
+
+            for (var i = 0; i < dataSplit.length; i+=4) {
+                for (var j = 0; j < dataSplit[i].length; j++) {
+                    if (dataSplit[i][j] == 0) {
+                        imgData.data[i+0]=255;
+                        imgData.data[i+1]=255;
+                        imgData.data[i+2]=255;
+                        imgData.data[i+3]=255;
+                    } else {
+                        imgData.data[i+0]=0;
+                        imgData.data[i+1]=0;
+                        imgData.data[i+2]=0;
+                        imgData.data[i+3]=255;
+                    }
+                }
+            }
+            ctx.putImageData(imgData,10,10);
+        },
+        ready: function() {
+            var xhr;
+            if (window.XMLHttpRequest) {
+                xhr = new XMLHttpRequest();
+            } else if (window.ActiveXObject) {
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+
+            xhr.onreadystatechange = () => {
+                this.binArray = xhr.responseText.split(",");
+            };
+            xhr.open("GET","../renderer/data");
+            xhr.send();
+        }
+    },
+    watch: {
+        binArray: function(val) {
+            this.drawCanvas();
         }
     }
 })
+
+app.ready();
+
+function reshape(array, rows, cols) {
+    var copy = array.slice(); // Copy all elements.
+
+    for (var r = 0; r < rows; r++) {
+        var row = [];
+        for (var c = 0; c < cols; c++) {
+            var i = r * cols + c;
+            if (i < copy.length) {
+                row.push(copy[i]);
+            }
+        }
+        copy.push(row);
+    }
+    return array;
+};
