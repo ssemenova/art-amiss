c589f575bea9f93b1c9ff71043f440258b4b49b2
Sofiya Semenova <novasofiya@gmail.com>
2016-10-15 14:14:14 -0400

    Template complete

---
+++ assets/js.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!',
+        curves: ["bla!", "bla2", "bla3"],
+        attributes: ["x-shift", "y-shift", "rotation", "x-stretch", "y-stretch"]
+    },
+    methods: {
+
+    }
+})
